f46274c3fb46651aa0c1a444ec28c8d8dfe22914
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-12-22 16:03:20 +0100

    first exercise works

--- server.py
+++ server.py
@@ -13,6 +13,19 @@ def upload_graph():
 @server.route('/http://127.0.0.1:5000/degrees_of_separation/<origin>/<destination>', methods=['POST'])
 def get_degrees_of_separation(graph, origin, destination): 
     
+    def find_path(graph, start, end, path=[], weight=0):
+    path = path + [{"node": start, "weight": weight}]
+    if start == end:
+        return path
+    if not start in graph:
+        return None
+    for conn in graph[start]:
+        if conn["node"] not in path:
+            newpath = find_path(graph, conn["node"], end, path, conn["weight"])
+            if newpath is not None:
+                return newpath
+
+    
     
     print(graph, origin, destination)
 server.run()
