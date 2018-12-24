2750149434b6a398d847fd1f5eb69cc7626d3732
yotroz <37019508+yotroz@users.noreply.github.com>
2018-12-23 23:23:28 -0100

    Update client.py

--- client.py
+++ client.py
@@ -9,12 +9,14 @@ Created on Fri Dec 21 23:22:03 2018
 #%%
 
 
-graph10 =  {
-            "a": ["b","c","d"],
-            "b": ["c"],
-            "c" : ["b", "d"],
-            "d" : ["b"],
-            }
+graph = {
+    "a": ["b", "c"],
+    "b": ["d"],
+    "c": ["d"],
+    "d": ["e"],
+    "e": [],
+    "f": []
+}
 
 import requests
 
@@ -25,10 +27,13 @@ def upload_graph(graph):
     request = requests.post(url .format('upload-graph'), json=data)
     
     return request.json()
- 
-def get_degrees_of_separation(graph, origin, destination): 
+
+
+
+def get_degrees_of_separation(origin, destination, graph): 
     data = graph
-    request = requests.post('http://127.0.0.1:5000/degrees_of_separation/<origin>/<destination>', json=data)
+    request = requests.put('http://127.0.0.1:5000/degrees-of-separation/{}/{}'.format(origin, destination) , json=data)
     
     return request.json()
+
     
